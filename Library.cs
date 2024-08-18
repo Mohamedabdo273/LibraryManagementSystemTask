3a4f4b87b7d41c28255aeaac03e2ac9fac1c5394
Mohamedabdo273 <mohamedsaleh200330@gmail.com>
2024-08-19 00:16:05 +0300

    first commit

---
+++ Library.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public class Library 
+    {
+        public  List<Book> books = new List<Book>();
+
+        public  string AddBook(Book book)
+        {
+            books.Add(book);
+            book.Availability = true;
+            return $"Book : {book.Title} added successfully";
+        }
+
+        public  string SearchBook(string search)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Title.ToLower().Contains(search.ToLower()) || book.Author.ToLower().Contains(search.ToLower()))
+                {
+                    if (book.Availability)
+                    {
+                        return $"Book : {book.Title} found";
+                    }
+                    else
+                    {
+                        return $"Book : {book.Title} not in library";
+                    }
+                }
+            }
+            return $"Book : {search} not found";
+        }
+
+        public  string BorrowBook(string title)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Title.ToLower().Contains(title.ToLower()) || book.Author.ToLower().Contains(title.ToLower()))
+                {
+                    if (book.Availability)
+                    {
+                        book.Availability = false;
+                        return $"Book : {book.Title} borrowed successfully";
+                    }
+                    else
+                    {
+                        return $"Book : {book.Title} is not in the library";
+                    }
+                }
+            }
+            return $"Book : {title} not found";
+        }
+
+        public  string ReturnBook(string title)
+        {
+            bool found = false;
+            foreach (Book book in books)
+            {
+                if (book.Title.ToLower().Contains(title.ToLower()) || book.Author.ToLower().Contains(title.ToLower()))
+                {
+                    if (!book.Availability)
+                    {
+                        book.Availability = true;
+                        return $"Book : {book.Title} returned successfully";
+                    }
+                    else
+                    {
+                        return $"Book : {book.Title} was not borrowed";
+                    }
+                }
+            }
+            return $"Book: {title} not found";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task4
+{
+    
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                 Library library = new Library();
+
+                // Adding books to the library
+                Console.WriteLine("Adding books to the library\n");
+                Console.WriteLine(library.AddBook(new Book("The Great Gatsby", "F. Scott Fitzgerald", "9780743273565")));
+                Console.WriteLine(library.AddBook(new Book("To Kill a Mockingbird", "Harper Lee", "9780061120084")));
+                Console.WriteLine(library.AddBook(new Book("1984", "George Orwell", "9780451524935")));
+                Console.WriteLine();
+
+                // Searching and borrowing books
+                Console.WriteLine("Searching and borrowing books...\n");
+                 Console.WriteLine(library.SearchBook("Gatsby"));
+                Console.WriteLine(library.SearchBook("George Orwell"));
+                Console.WriteLine(library.SearchBook("Mockingbird"));
+                Console.WriteLine();
+                Console.WriteLine(library.BorrowBook("The Great"));
+                Console.WriteLine(library.BorrowBook("1984"));
+                Console.WriteLine(library.BorrowBook("Harry Potter"));// This book is not in the library
+                Console.WriteLine(library.BorrowBook("The Great Gatsby"));// This book is not in the library
+                Console.WriteLine(library.BorrowBook("1984"));
+
+                // Returning books
+                Console.WriteLine("\nReturning books...\n");
+                 Console.WriteLine(library.ReturnBook("The Great Gatsby"));
+                Console.WriteLine(library.ReturnBook("Harry Potter")); // This book is not borrowed
+
+                Console.ReadLine();
+
+            }
+        }
+    
+}
+
